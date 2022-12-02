eb26f4b49e2854cf010b9ec3102558b77e8217c2
Flederossi <oskar.web.thieme@gmail.com>
2022-12-02 17:31:48 +0100

    Solved day2

--- years/2022/day2/main_day2_task1.py
+++ years/2022/day2/main_day2_task1.py
@@ -1,7 +1,21 @@
 __author__ = "Flederossi"
 
+values = {"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
+
+win = [2, 3, 1]
+
 def main(data):
-	pass
+	splitted = [[values[element.split(" ")[0]], values[element.split(" ")[1]]] for element in data.split("\n")]
+
+	res = 0
+	for element in splitted:
+		res += element[1]
+		if win[element[0] - 1] == element[1]:
+			res += 6
+		elif element[0] == element[1]:
+			res += 3
+
+	print(res)
 
 if __name__ == "__main__":
 	with open("input.txt", "r") as f:
--- years/2022/day2/main_day2_task2.py
+++ years/2022/day2/main_day2_task2.py
@@ -1,7 +1,28 @@
 __author__ = "Flederossi"
 
+values = {"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
+
+win = [2, 3, 1]
+lose = [3, 1, 2]
+
 def main(data):
-	pass
+	splitted = [[values[element.split(" ")[0]], values[element.split(" ")[1]]] for element in data.split("\n")]
+
+	res = 0
+	for element in splitted:
+		choose = 0
+		if element[1] == 1:
+			choose = lose[element[0] - 1]
+		elif element[1] == 2:
+			choose = element[0]
+			res += 3
+		else:
+			choose = win[element[0] - 1]
+			res += 6
+
+		res += choose
+
+	print(res)
 
 if __name__ == "__main__":
 	with open("input.txt", "r") as f:
