b09fde1e65c20beca6595d99970a3fef2fee505f
Flederossi <oskar.web.thieme@gmail.com>
2022-12-03 08:04:58 +0100

    Solved day3

--- years/2022/day3/main_day3_task2.py
+++ years/2022/day3/main_day3_task2.py
@@ -1,7 +1,15 @@
 __author__ = "Flederossi"
 
 def main(data):
-	pass
+	splitted = data.split("\n")
+	splitted = [list(set(splitted[i]).intersection(splitted[i+1], splitted[i+2]))[0] for i in range(0, len(splitted), 3)]
+
+	res = 0
+	for element in splitted:
+		s = [38, 96]
+		res += ord(element) - s[int(ord(element) >= 97)] 
+
+	print(res)
 
 if __name__ == "__main__":
 	with open("input.txt", "r") as f:
