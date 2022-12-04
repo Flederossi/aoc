30693dc6407c3e387f866b33e806fd801bd30080
Flederossi <oskar.web.thieme@gmail.com>
2022-12-04 14:18:29 +0100

    Solved day4

--- years/2022/day4/main_day4_task1.py
+++ years/2022/day4/main_day4_task1.py
@@ -1,7 +1,16 @@
 __author__ = "Flederossi"
 
 def main(data):
-	pass
+	splitted = [[[int(elve.split("-")[0]), int(elve.split("-")[1])] for elve in line.split(",")] for line in data.split("\n")]
+
+	res = 0
+	for pair in splitted:
+		for j in range(0, 2):
+			if pair[j][0] <= pair[-j+1][1] and pair[j][0] >= pair[-j+1][0] and pair[j][1] <= pair[-j+1][1] and pair[j][1] >= pair[-j+1][0]:
+				res += 1
+				break
+
+	print(res)
 
 if __name__ == "__main__":
 	with open("input.txt", "r") as f:
