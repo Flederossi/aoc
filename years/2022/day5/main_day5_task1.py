2c964069e0e16f4f043a29c11b6ce5cdc8683c67
Flederossi <oskar.web.thieme@gmail.com>
2022-12-05 08:15:44 +0100

    Solved day5

--- years/2022/day5/main_day5_task1.py
+++ years/2022/day5/main_day5_task1.py
@@ -1,7 +1,38 @@
 __author__ = "Flederossi"
 
 def main(data):
-	pass
+	splitted = data.split("\n")
+
+	tempcrates = [splitted[i].replace("   ", "[-]").replace(" ", "").split("][") for i in range(0, splitted.index("") - 1)]
+	moves = [splitted[i].split(" ") for i in range(splitted.index("") + 1, len(splitted))]
+
+	for i in range(len(tempcrates)):
+		tempcrates[i][0] = tempcrates[i][0][1:]
+		tempcrates[i][-1] = tempcrates[i][-1][:-1]
+
+	crates = [[] for i in range(len(tempcrates[0]) - 1)]
+	for i in range(len(tempcrates) - 1, -1, -1):
+		for j in range(0, len(tempcrates[0]) - 1):
+			if tempcrates[i][j] != "-":
+				crates[j].append(tempcrates[i][j])
+
+	keywords = ["move", "from", "to"]
+	for i in range(len(moves)):
+		for word in keywords:
+			moves[i].remove(word)
+
+	for move in moves:
+		for i in range(int(move[0])):
+			column1 = int(move[1]) - 1
+			column2 = int(move[2]) - 1
+			if len(crates[column1]) > 0:
+				crates[column2].append(crates[column1][-1])
+				del crates[column1][-1]
+
+	res = ""
+	for stack in crates:
+		res += stack[-1]
+	print(res)
 
 if __name__ == "__main__":
 	with open("input.txt", "r") as f:
--- years/2022/day5/main_day5_task2.py
+++ years/2022/day5/main_day5_task2.py
@@ -1,7 +1,37 @@
 __author__ = "Flederossi"
 
 def main(data):
-	pass
+	splitted = data.split("\n")
+
+	tempcrates = [splitted[i].replace("   ", "[-]").replace(" ", "").split("][") for i in range(0, splitted.index("") - 1)]
+	moves = [splitted[i].split(" ") for i in range(splitted.index("") + 1, len(splitted))]
+
+	for i in range(len(tempcrates)):
+		tempcrates[i][0] = tempcrates[i][0][1:]
+		tempcrates[i][-1] = tempcrates[i][-1][:-1]
+
+	crates = [[] for i in range(len(tempcrates[0]) - 1)]
+	for i in range(len(tempcrates) - 1, -1, -1):
+		for j in range(0, len(tempcrates[0]) - 1):
+			if tempcrates[i][j] != "-":
+				crates[j].append(tempcrates[i][j])
+
+	keywords = ["move", "from", "to"]
+	for i in range(len(moves)):
+		for word in keywords:
+			moves[i].remove(word)
+
+	for move in moves:
+		column1 = int(move[1]) - 1
+		column2 = int(move[2]) - 1
+		for element in crates[column1][-int(move[0]):]:
+			crates[column2].append(element)
+		del crates[column1][-int(move[0]):]
+
+	res = ""
+	for stack in crates:
+		res += stack[-1]
+	print(res)
 
 if __name__ == "__main__":
 	with open("input.txt", "r") as f:
