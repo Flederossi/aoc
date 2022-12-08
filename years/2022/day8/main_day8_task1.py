c4231d04a4f4f2dd50cf4c2a34f0ebe5cf65e7b8
Flederossi <oskar.web.thieme@gmail.com>
2022-12-08 17:52:30 +0100

    Solved day8

--- years/2022/day8/main_day8_task1.py
+++ years/2022/day8/main_day8_task1.py
@@ -1,7 +1,33 @@
 __author__ = "Flederossi"
 
 def main(data):
-	pass
+	res = 0
+	splitted = [list(element) for element in data.split("\n")]
+	visible = [[False for i in range(len(splitted[0]))] for j in range(len(splitted))]
+
+	h = [range(len(splitted)), range(len(splitted) - 1, -1, -1)]
+	for element in h:
+		for x in range(0, len(splitted[0])):
+			maxi = -1
+			for y in element:
+				if int(splitted[y][x]) > maxi:
+					if visible[y][x] == False:
+						res += 1
+					visible[y][x] = True
+					maxi = int(splitted[y][x])
+
+	v = [range(len(splitted[0])), range(len(splitted[0]) - 1, -1, -1)]
+	for element in v:
+		for y in range(0, len(splitted)):
+			maxi = -1
+			for x in element:
+				if int(splitted[y][x]) > maxi:
+					if visible[y][x] == False:
+						res += 1
+					visible[y][x] = True
+					maxi = int(splitted[y][x])
+
+	print(res)
 
 if __name__ == "__main__":
 	with open("input.txt", "r") as f:
--- years/2022/day8/main_day8_task2.py
+++ years/2022/day8/main_day8_task2.py
@@ -1,7 +1,33 @@
+import numpy
+
 __author__ = "Flederossi"
 
+def value_check(i, a, splitted, x, y):
+	check = [[y, x + a], [y, x - a], [y + a, x], [y - a, x]]
+	return splitted[check[i][0]][check[i][1]]
+
 def main(data):
-	pass
+	splitted = [list(element) for element in data.split("\n")]
+	score = 0
+
+	for y in range(len(splitted)):
+		for x in range(len(splitted[0])):
+			mult = [0, 0, 0, 0]
+			ranges = [
+				range(1, len(splitted[0]) - x),
+				range(1, x + 1),
+				range(1, len(splitted) - y),
+				range(1, y + 1)
+			]
+			for i in range(len(ranges)):
+				for a in ranges[i]:
+					mult[i] += 1
+					if splitted[y][x] <= value_check(i, a, splitted, x, y):
+						break
+
+			score = max(score, numpy.prod(mult))
+
+	print(score)
 
 if __name__ == "__main__":
 	with open("input.txt", "r") as f:
