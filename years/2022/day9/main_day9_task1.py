982e721f17321a3aacaf97e41429b176d7bb9772
Flederossi <oskar.web.thieme@gmail.com>
2022-12-09 16:38:24 +0100

    Solved day9

--- years/2022/day9/main_day9_task1.py
+++ years/2022/day9/main_day9_task1.py
@@ -1,7 +1,34 @@
 __author__ = "Flederossi"
 
 def main(data):
-	pass
+	d = {
+		"R": [1, 0],
+		"L": [-1, 0],
+		"D": [0, 1],
+		"U": [0, -1]
+	}
+
+	h_pos = (0, 0)
+	t_pos = (0, 0)
+	visited = set()
+
+	splitted = [line.split(" ") for line in data.split("\n")]
+
+	for move in splitted:
+		for i in range(int(move[1])):
+			visited.add(t_pos)
+			h_pos = (h_pos[0] + d[move[0]][0], h_pos[1] + d[move[0]][1])
+			
+			dr = (h_pos[0]-t_pos[0])
+			dc = (h_pos[1]-t_pos[1])
+
+			if abs(dr) >= 2:
+				t_pos = (h_pos[0] - 1 if t_pos[0] < h_pos[0] else h_pos[0] + 1, h_pos[1])
+			elif abs(dc) >= 2:
+				t_pos = (h_pos[0], h_pos[1] - 1 if t_pos[1] < h_pos[1] else h_pos[1] + 1)
+
+	print(len(visited))
+
 
 if __name__ == "__main__":
 	with open("input.txt", "r") as f:
--- years/2022/day9/main_day9_task2.py
+++ years/2022/day9/main_day9_task2.py
@@ -1,7 +1,43 @@
 __author__ = "Flederossi"
 
+def follow(h, t):
+	dr = (h[0]-t[0])
+	dc = (h[1]-t[1])
+
+	if abs(dr) >= 2 and abs(dc) >= 2:
+		t = (h[0] - 1 if t[0] < h[0] else h[0] + 1, h[1] - 1 if t[1] < h[1] else h[1] + 1)
+	elif abs(dr) >= 2:
+		t = (h[0] - 1 if t[0] < h[0] else h[0] + 1, h[1])
+	elif abs(dc) >= 2:
+		t = (h[0], h[1] - 1 if t[1] < h[1] else h[1] + 1)
+
+	return t
+
 def main(data):
-	pass
+	d = {
+		"R": [1, 0],
+		"L": [-1, 0],
+		"D": [0, 1],
+		"U": [0, -1]
+	}
+
+	h_pos = (0, 0)
+	t_pos = [(0, 0) for _ in range(9)]
+	visited = set()
+
+	splitted = [line.split(" ") for line in data.split("\n")]
+
+	for move in splitted:
+		for _ in range(int(move[1])):
+			h_pos = (h_pos[0] + d[move[0]][0], h_pos[1] + d[move[0]][1])
+			
+			t_pos[0] = follow(h_pos, t_pos[0])
+			for i in range(1, 9):
+				t_pos[i] = follow(t_pos[i-1], t_pos[i])
+			visited.add(t_pos[8])
+
+	print(len(visited))
+
 
 if __name__ == "__main__":
 	with open("input.txt", "r") as f:
