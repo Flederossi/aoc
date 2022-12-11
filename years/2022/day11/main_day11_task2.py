710c0b3dadd84216a08d4a00b873fadcaff96a32
Flederossi <oskar.web.thieme@gmail.com>
2022-12-11 15:47:25 +0100

    Solved day11

--- years/2022/day11/main_day11_task2.py
+++ years/2022/day11/main_day11_task2.py
@@ -1,7 +1,52 @@
+import math
+
 __author__ = "Flederossi"
 
+class Monkey:
+	def __init__(self, items, operation, test, true, false):
+		self.items = items
+		self.operation = operation
+		self.test = test
+		self.true = true
+		self.false = false
+		self.inspect = 0
+
 def main(data):
-	pass
+	monkeys = []
+	splitted = [element.split("\n") for element in data.split("\n\n")]
+	div = 1
+	for monkey in splitted:
+		monkeys.append(
+			Monkey(
+				[int(item) for item in monkey[1].replace(" ", "").split(":")[1].split(",")],
+				monkey[2].replace(" ", "").split("=")[1],
+				int(monkey[3].replace(" ", "").split("by")[1]),
+				int(monkey[4][-1]),
+				int(monkey[5][-1])
+			)
+		)
+		test = int(monkey[3].replace(" ", "").split("by")[1])
+		div *= (test * div) // math.gcd(div, test)
+
+	for _ in range(10000):
+		for monkey in monkeys:
+			for item in monkey.items:
+				monkey.inspect += 1
+				new = eval(monkey.operation.replace("old", str(item))) % div
+				if new % monkey.test == 0:
+					monkeys[monkey.true].items.append(new)
+				else:
+					monkeys[monkey.false].items.append(new)
+			monkey.items.clear()
+
+	res = []
+	for monkey in monkeys:
+		res.append(monkey.inspect)
+	maxi = max(res)
+	del res[res.index(maxi)]
+	maxi *= max(res)
+	print(maxi)
+
 
 if __name__ == "__main__":
 	with open("input.txt", "r") as f:
