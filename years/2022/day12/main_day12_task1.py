8bd03488f5473bdb68086246be9cf0eb6ae1b0f7
Flederossi <oskar.web.thieme@gmail.com>
2022-12-12 19:55:32 +0100

    Solved day12

--- years/2022/day12/main_day12_task1.py
+++ years/2022/day12/main_day12_task1.py
@@ -1,7 +1,33 @@
 __author__ = "Flederossi"
 
 def main(data):
-	pass
+	splitted = [list(line) for line in data.split("\n")]
+	
+	for y in range(len(splitted)):
+		for x in range(len(splitted[0])):
+			if splitted[y][x] == "S":
+				start = (x, y)
+				splitted[y][x] = "a"
+			elif splitted[y][x] == "E":
+				end = (x, y)
+				splitted[y][x] = "z"
+    
+	path = [(0, end)]
+	visited = set()
+	while True:
+		l, pos = path.pop(0)
+		if pos in visited:
+			continue
+		visited.add(pos)
+		if splitted[pos[1]][pos[0]] == "a":
+			if pos == start:
+				print(l)
+				break
+		
+		for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+			newpos = (pos[0] + dx, pos[1] + dy)
+			if 0 <= newpos[1] < len(splitted) and 0 <= newpos[0] < len(splitted[0]) and newpos not in visited and (ord(splitted[pos[1]][pos[0]]) - ord(splitted[newpos[1]][newpos[0]])) <= 1:
+				path.append((l+1, newpos))
 
 if __name__ == "__main__":
 	with open("input.txt", "r") as f:
